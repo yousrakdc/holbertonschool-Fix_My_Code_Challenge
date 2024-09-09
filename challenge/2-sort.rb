268f1605417a9c65dad044c2daa9d13da0f4f73c
yousrakdc <yousrakdc@gmail.com>
2024-09-09 11:07:04 +0200

    update

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -7,14 +7,11 @@
 result = []
 
 ARGV.each do |arg|
-  # Skip if not integer
   next unless arg =~ /^-?\d+$/
 
-  # Convert to integer and push to result array
   result << arg.to_i
 end
 
-# Sort the result array in ascending order
 result.sort!
 
 puts result
