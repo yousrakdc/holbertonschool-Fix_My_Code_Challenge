7b07a41de881ca130496dceb21383b1b809df0ac
yousrakdc <yousrakdc@gmail.com>
2024-09-09 11:08:11 +0200

    update

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -5,13 +5,23 @@
 ###
 
 result = []
-
 ARGV.each do |arg|
-  next unless arg =~ /^-?\d+$/
+    next if arg !~ /^-?\d+$/
 
-  result << arg.to_i
+    i_arg = arg.to_i
+    
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+        end
+    end
+    result << i_arg if !is_inserted
 end
-
-result.sort!
 
 puts result
